12d072d776e9d139e351bb879559551d935a08af
Daniel Shapira <danielshapira3@gmail.com>
2025-01-06 10:24:32 +0200

    added electron

---
+++ electron/main.js
@@ -0,0 +1,54 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const fs = require("fs-extra");
+
+const dataFilePath = path.join(
+  app.getPath("C:\\Chrome downloads"),
+  "data.json"
+);
+
+let mainWindow;
+
+// Electron browser window
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  // loading the react application
+  mainWindow.loadURL("http://localhost:5173");
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+};
+
+ipcMain.handle("load-data", async () => {
+  try {
+    const data = await fs.readJson(dataFilePath);
+    return data;
+  } catch (error) {
+    console.error("Error reading JSON file:", error.message);
+    return [];
+  }
+});
+ipcMain.handle("save-data", async (_, data) => {
+  await fs.writeJson(dataFilePath, data);
+});
+
+// Electron lifecycle events
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
